56d582835dacc7f9d043b333cf7c73ba60a5e25c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-01-01 19:47:14 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MoodController;
+use App\Http\Controllers\EntryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::resource('moods', MoodController::class);
+Route::resource('entries', EntryController::class);
